557b0ec232094041ebf8ed1ee7f88eb97e0a935a
Vadzim <vadzimk@hotmail.com>
2021-01-08 23:33:09 -0800

    completed 4.5 helper functions and unit tests step3

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,9 +1,9 @@
-import {dummy, totalLikes} from "../utils/list_helper.js";
+import listHelper from "../utils/list_helper.js";
 
 
 test('dummy returns one', ()=>{
     const blogs = []
-    const result = dummy(blogs)
+    const result = listHelper.dummy(blogs)
     expect(result).toBe(1)
 })
 
@@ -20,7 +20,52 @@ describe("total likes", ()=>{
     ]
 
     test('when list has only one blog, equals the likes of that', ()=>{
-        const result = totalLikes(listWithOneBlog)
+        const result = listHelper.totalLikes(listWithOneBlog)
         expect(result).toBe(5)
+    })
+})
+
+
+describe("favorite blog", ()=>{
+
+    const listWithMany = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 1,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'You can run a single test with',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            likes: 12
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'helper functions and unit tests',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 7,
+            __v: 0
+        }
+    ]
+
+    test('find favorite blog', ()=>{
+        const result = listHelper.favoriteBlog(listWithMany)
+        expect(result).toEqual({
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            likes: 12
+        })
     })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,21 @@ const totalLikes = (blogs) => {
     return blogs.reduce((acc, curr) => acc + curr.likes, 0)
 }
 
+const favoriteBlog = blogs => {
+    const max = blogs.reduce((acc, curr) => curr.likes > acc ? curr.likes : acc, 0)
+
+    const obj = blogs.find(e => e.likes === max)
+
+    return {
+        title: obj.title,
+        author: obj.author,
+        likes: obj.likes
+    }
+
+}
 
 
-export {dummy, totalLikes}
+
+const listHelper = {dummy, totalLikes, favoriteBlog}
+export default listHelper
 
