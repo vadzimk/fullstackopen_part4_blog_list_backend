21b34933ef9798dcc76982e1299337ba623aa443
Vadzim <vadzimk@hotmail.com>
2021-01-09 22:09:54 -0800

    completed 4.6 helper functions and unit tests step4

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -68,4 +68,47 @@ describe("favorite blog", ()=>{
             likes: 12
         })
     })
+
+})
+
+describe('most blogs', ()=>{
+
+    const listWithMany = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 1,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'You can run a single test with',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            likes: 12
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'helper functions and unit tests',
+            author: 'Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 7,
+            __v: 0
+        }
+    ]
+
+
+    test('most blogs', ()=>{
+        const result = listHelper.mostBlogs(listWithMany)
+        expect(result).toEqual({ author: 'Edsger W. Dijkstra', blogs: 3 }
+        )
+    })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -19,9 +19,68 @@ const favoriteBlog = blogs => {
     }
 
 }
+/**
+ * @param blogs is array of blogs
+ * @return object with parameters:
+ * {author, blogs}
+ * author who has most blogs and number of his blogs
+ * */
+const mostBlogs = (blogs)=>{
+    const authors = []
+    blogs.forEach(blog=>{
+        const authorObj = authors.find(item=>item.author===blog.author)
+        if (!authorObj){
+            authors.push({
+                author: blog.author,
+                blogs: 1
+            }, )
+        } else {
+            const index = authors.indexOf(authorObj)
+            authors[index].blogs +=1
+        }
+    })
+    if (authors.length>0)
+        return authors.sort()[0]
+    else
+        return {}
+}
 
 
+const listWithMany = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 1,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'You can run a single test with',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    },
+    {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        likes: 12
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'helper functions and unit tests',
+        author: 'Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 7,
+        __v: 0
+    }
+]
 
-const listHelper = {dummy, totalLikes, favoriteBlog}
+const res = mostBlogs(listWithMany)
+console.log(res)
+
+const listHelper = {dummy, totalLikes, favoriteBlog, mostBlogs}
 export default listHelper
 
